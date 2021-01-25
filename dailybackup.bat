8ecdcf9ea77fd2737071c37a40e7c0714d29ac6c
Daani B <awesomebrick@hotmail.com>
2021-01-25 15:23:10 -0600

    i wanna cry, at least it copies over files and folders now.

--- dailybackup.bat
+++ dailybackup.bat
@@ -1,10 +1,15 @@
-@ECHO OFF
+REM @ECHO OFF
 
 set currentdate=%date:~4,10%
 set currentdate=%currentdate:/=-%
 
-cd "D:\backups\weekly"
-mkdir %currentdate%
-cd %currentdate%
+cd /D D:\backups\weekly
+REM mkdir %currentdate%
+REM cd %currentdate%
 
-xcopy "C:\Desktop\minecraftserverold\world*" /s /e /v
+robocopy "C:\Users\dan\Desktop\minecraftserverold\world" "D:\backups\weekly\%currentdate%\world" /e
+robocopy "C:\Users\dan\Desktop\minecraftserverold\world_nether\" "D:\backups\weekly\%currentdate%\world_nether\" /e
+robocopy "C:\Users\dan\Desktop\minecraftserverold\world_the_end\" "D:\backups\weekly\%currentdate%\world_the_end\" /e
+
+REM cd ..
+REM cd /d "C:\Users\dan\Desktop\backup scripts"
